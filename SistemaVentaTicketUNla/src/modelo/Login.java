e8398f31a6a4b3547ef9dc2f363892713328a6ae
alan-PC\alan <alan.perez536@outlook.es>
2018-09-12 18:01:05 -0300

    algunas clases subidas

---
+++ SistemaVentaTicketUNla/src/modelo/Login.java
@@ -0,0 +1,37 @@
+package modelo;
+
+public class Login {
+	private String usuario;
+	private String contraseña;
+	private Privilegio privilegio;
+	public Login(String usuario, String contraseña, Privilegio privilegio) {
+		super();
+		this.usuario = usuario;
+		this.contraseña = contraseña;
+		this.privilegio = privilegio;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	public Privilegio getPrivilegio() {
+		return privilegio;
+	}
+	public void setPrivilegio(Privilegio privilegio) {
+		this.privilegio = privilegio;
+	}
+	@Override
+	public String toString() {
+		return "Login [usuario=" + usuario + ", contraseña=" + contraseña + ", privilegio=" + privilegio + "]";
+	}
+	
+
+}
